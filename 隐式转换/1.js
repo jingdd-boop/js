7524f5f3da3eb21114c3d1ad85750dc7e3500fce
jingdd-boop <1736929925@qq.com>
2020-06-26 12:14:05 +0800

    js 5

---
+++ 隐式转换/1.js
@@ -0,0 +1,21 @@
+// console.log(1 + "true");//1true
+// console.log(1 + true);//2
+// console.log(1 + undefined);//NaN
+// console.log(1 + null);//1
+
+console.log("2" > 10);//false
+console.log("2" > "10");
+console.log("abc" > "b");//false
+console.log("abc" > "aad");
+console.log(NaN == NaN);//false
+console.log(undefined == null);
+console.log(undefined == undefined);
+console.log(null == null);
+console.log(NaN == undefined);//false
+console.log(NaN == 0);//false
+
+
+
+
+
+

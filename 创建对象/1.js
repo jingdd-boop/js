0a94c7f354210430b7dca30d09756a5c7d2c058e
jingdd-boop <1736929925@qq.com>
2020-08-16 10:45:23 +0800

    js

--- 创建对象/1.js
+++ 创建对象/1.js
@@ -11,15 +11,62 @@
 
 // var person1 = createPerson("jing",21,"web");
 // var person2 = createPerson("hao",21,"java");
+// function Person(name,age,job){
+//   this.name = name;
+//   this.age = age;
+//   this.job = job;
+//   this.sayName = function(){
+//     alert(this.name);
+//   };
+
+// }
+
+// var person1 = new Person("jing",21,"web");
+// var person2 = new ePerson("hao",21,"java");
+
+// function Person(){
+// }
+
+// Person.prototype.name = 'jing';
+// Person.prototype.age = '21';
+// Person.prototype.job = 'stu';
+// Person.prototype.sayName = function(){
+//   console.log(this.name);
+// };
+
+// var person1 = new Person();
+// person1.sayName();
+// var person2 = new Person();
+// person2.sayName();
+
+// console.log(person1.sayName == person2.sayName);
+// console.log(Person.prototype.isPrototypeOf(person1));
+// console.log(Person.prototype.isPrototypeOf(person2));
+// console.log(Object.getPrototypeOf(person1) == Person.prototype)
+// console.log(Object.getPrototypeOf(person2) == Person.prototype)
+
+
+
 function Person(name,age,job){
   this.name = name;
   this.age = age;
   this.job = job;
-  this.sayName = function(){
-    alert(this.name);
-  };
+  this.friends = ['wang','zhi'];
+}
 
+Person.prototype = {
+  constructor:Person,
+  sayName:function(){
+    console.log(this.name);
+  }
 }
 
 var person1 = new Person("jing",21,"web");
-var person2 = new ePerson("hao",21,"java");
+var person2 = new Person("hao",21,"java");
+
+person1.friends.push('hao');
+console.log(person1.friends);
+console.log(person2.friends);
+console.log(person1.friends === person2.friends);
+console.log(person1.sayName === person2.sayName);
+
